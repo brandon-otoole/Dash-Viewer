b5ffe546733c7b2c24e40397d390089590c9999f
Brandon O'Toole <otoolb@gmail.com>
2014-03-20 10:50:18 -0700

    cleaned up formatting

--- DashView.py
+++ DashView.py
@@ -1,6 +1,5 @@
 from Tkinter import *
 import math
-from threading import Timer
  
 class Screen(Frame):
     def __init__(self, master):
@@ -20,16 +19,22 @@ class Screen(Frame):
         self.canvasWidth = 700
         self.canvasHeight = 500
         
-        self.w = Canvas(master, width=self.canvasWidth, height=self.canvasHeight)
+        self.w = Canvas(master, width=self.canvasWidth, 
+                        height=self.canvasHeight)
         self.w.grid(row=0, column=0, columnspan=4)
 
-        self.lArrowToggle = Button(master, text="LEFT",command=self.toggleLeft)
+        self.lArrowToggle = Button(master, text="LEFT", command=self.toggleLeft)
         self.lArrowToggle.grid(row=1, column=0)
-        self.sbTotalDistance = Spinbox(master, from_ = 0, to = 100, increment=5, command=self.toggleTotal)
+        
+        self.sbTotalDistance = Spinbox(master, from_ = 0, to = 100, 
+                                       increment=5, command=self.toggleTotal)
         self.sbTotalDistance.grid(row=1, column=1)
-        self.sbNextDistance = Spinbox(master, from_ = 0.0, to = 5.0, increment=0.1, command=self.toggleNext)
+        
+        self.sbNextDistance = Spinbox(master, from_ = 0.0, to = 5.0, 
+                                      increment=0.1, command=self.toggleNext)
         self.sbNextDistance.grid(row=1, column=2)
-        self.rArrowToggle = Button(master, text="RIGHT",command=self.toggleRight)
+        
+        self.rArrowToggle = Button(master, text="RIGHT", command=self.toggleRight)
         self.rArrowToggle.grid(row=1, column=3)
         
         self.updateScreen()
@@ -65,13 +70,16 @@ class Screen(Frame):
         self.updateScreen()
     
     def drawLeftArrow(self, x, y, w, h, color="white"):
-        self.w.create_polygon(x+w, y-h/2, x, y, x+w, y+h/2, x+w/5, y, fill=color, outline=color)
+        self.w.create_polygon(x+w, y-h/2, x, y, x+w, y+h/2, x+w/5, y, 
+                              fill=color, outline=color)
     
     def drawRightArrow(self, x, y, w, h, color="white"):
-        self.w.create_polygon(x-w, y-h/2, x, y, x-w, y+h/2, x-w/5, y, fill=color, outline=color)
+        self.w.create_polygon(x-w, y-h/2, x, y, x-w, y+h/2, x-w/5, y, 
+                              fill=color, outline=color)
     
     def drawTotalDistance(self, x, y, color="white"):
-        canvas_id = self.w.create_text(x, y, anchor="nw", fill=color, font="Helvetica 18")
+        canvas_id = self.w.create_text(x, y, anchor="nw", fill=color, 
+                                       font="Helvetica 18")
         self.w.itemconfig(canvas_id, text="TOTAL DISTANCE:  miles")
         self.w.insert(canvas_id, 16, self.totalDistance)
         
@@ -81,10 +89,11 @@ class Screen(Frame):
     
     def clearScreen(self):
         color = "black"
-        self.w.create_rectangle(0, 0, self.canvasWidth, self.canvasHeight, fill=color, outline=color)
+        self.w.create_rectangle(0, 0, self.canvasWidth, self.canvasHeight, 
+                                fill=color, outline=color)
 
-screen = Screen( Tk() )
-mainloop()
-
-
+if __name__ == "__main__":
+    #main(sys.argv[1:])
+    screen = Screen( Tk() )
+    mainloop()
 
